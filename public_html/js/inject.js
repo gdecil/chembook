a664f41b416fd71fbab2e134585a89d096053fd2
decilgi0 <gdecil@gmail.com>
2014-10-24 16:45:27 +0200

    / ‘public_html/js/inject.js’

--- public_html/js/inject.js
+++ public_html/js/inject.js
@@ -125,6 +125,7 @@ function appendMain(page) {
             });
         });
         if (server.indexOf("localhost") == -1) {
+        	var prs 0 getProjects()
             var dataOb = JSON.parse(getProjects());
             var mydata = [];
             dataOb.forEach(function (item) {
