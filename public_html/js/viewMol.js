da1e65019c36ae9dfbdb23b00e0a08d8465000a9
gippoLinux <gdecil@gmail.com>
2014-10-25 17:24:09 +0200

    / ‘public_html/js/viewMol.js’

--- public_html/js/viewMol.js
+++ public_html/js/viewMol.js
@@ -2,6 +2,9 @@ var server = window.location.protocol + "//" + window.location.host;
 if (server == "http://10.206.89.79"){
 	server = "http://10.206.89.79:5000"
 }
+if (server == "http://localhost"){
+	server = "http://localhost:5000"
+}
 
 var exp;
 var expCurrent;
