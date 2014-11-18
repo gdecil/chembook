96aa937bd7ce08feb118e7a1025f51e873897646
decilgi0 <gdecil@gmail.com>
2014-11-18 16:43:13 +0100

    / ‘public_html/js/nodeServer.js’

--- public_html/js/nodeServer.js
+++ public_html/js/nodeServer.js
@@ -2,7 +2,17 @@ var http = require('http');
  
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Worldn');
+  var zerorpc = require("zerorpc");
+
+  var client = new zerorpc.Client();
+  client.connect("tcp://127.0.0.1:4242");
+
+  client.invoke("hello", "World!", function(error, res, more) {
+    console.log(res);
+    res.end('Hello Worldn');
+  });
+
+  
 })
  
 server.listen(1337, '0.0.0.0');
