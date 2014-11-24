6457a9412bacf0bf05797877824206363fd7d5cc
decilgi0 <gdecil@gmail.com>
2014-11-24 11:36:32 +0100

    / ‘indigo-gdecil/indigo/wsgi/test.py’

---
+++ indigo-gdecil/indigo/wsgi/test.py
@@ -0,0 +1,45 @@
+import zerorpc
+from indigo import *
+from indigo_renderer import *
+from tempfile import *
+from shutil import copyfileobj
+import uuid
+from serial.rfc2217 import ECHO
+
+    def renderInd(self, smile):
+        
+        typeInd =""
+        indigo = Indigo()
+        if typeInd == "rea":
+            mol1 = indigo.loadReaction(smile)
+        else:
+            print smile
+            mol1 = indigo.loadMolecule(smile)
+            print mol1
+        
+        renderer = IndigoRenderer(indigo);
+        print renderer
+        indigo.setOption("render-output-format", "png");
+        indigo.setOption("render-margins", 10, 10);
+
+        datadir = ""
+            
+        unique_filename= str(uuid.uuid4()) + ".png"
+    
+        renderer.renderToFile(mol1, datadir + unique_filename);
+        print datadir + unique_filename
+        print "uno"
+            
+        tempFileObj = NamedTemporaryFile(mode='w+b',suffix='png')
+        pilImage = open(datadir + unique_filename,'rb')
+        copyfileobj(pilImage,tempFileObj)
+        pilImage.close()
+        print "due"
+#         remove(datadir + unique_filename)
+        tempFileObj.seek(0,0)
+        response = send_file(tempFileObj)
+        print unique_filename
+        return response
+    def hello(self, name):
+        print self
+        return "Hello, %s" % name
