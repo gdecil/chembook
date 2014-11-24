4a1921351fc0f56a239b24b3e012a35d337ffe8c
decilgi0 <gdecil@gmail.com>
2014-11-24 09:52:04 +0100

    / ‘indigo-gdecil/indigo/wsgi/rpc.py’

--- indigo-gdecil/indigo/wsgi/rpc.py
+++ indigo-gdecil/indigo/wsgi/rpc.py
@@ -7,35 +7,43 @@ import uuid
 from serial.rfc2217 import ECHO
 
 class HelloRPC(object):
-    def renderInd(self, smile, typeInd):
+    def renderInd(self, smile):
+        
+        typeInd =""
         indigo = Indigo()
         if typeInd == "rea":
             mol1 = indigo.loadReaction(smile)
         else:
+            print smile
             mol1 = indigo.loadMolecule(smile)
-    
+            print mol1
+        
         renderer = IndigoRenderer(indigo);
+        print renderer
         indigo.setOption("render-output-format", "png");
         indigo.setOption("render-margins", 10, 10);
-        if _platform == "Linux-3.13.0-36-generic-i686-with-Ubuntu-14.04-trusty":
-            datadir = ""        
-        elif _platform == "Linux-3.13.0-39-generic-i686-with-Ubuntu-14.04-trusty":
-            datadir = ""        
-        else:
-            datadir = os.environ['OPENSHIFT_DATA_DIR']
+
+        datadir = ""
             
         unique_filename= str(uuid.uuid4()) + ".png"
     
         renderer.renderToFile(mol1, datadir + unique_filename);
+        print datadir + unique_filename
+        print "uno"
             
         tempFileObj = NamedTemporaryFile(mode='w+b',suffix='png')
         pilImage = open(datadir + unique_filename,'rb')
         copyfileobj(pilImage,tempFileObj)
         pilImage.close()
-        remove(datadir + unique_filename)
+        print "due"
+#         remove(datadir + unique_filename)
         tempFileObj.seek(0,0)
         response = send_file(tempFileObj)
+        print unique_filename
         return response
+    def hello(self, name):
+        print self
+        return "Hello, %s" % name
 
 s = zerorpc.Server(HelloRPC())
 s.bind("tcp://0.0.0.0:4242")
