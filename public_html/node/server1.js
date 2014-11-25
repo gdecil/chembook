405a36ec91510b0728ac39823939f168f6e41ea4
decilgi0 <gdecil@gmail.com>
2014-11-25 14:42:58 +0100

    / ‘public_html/node/server1.js’

--- public_html/node/server1.js
+++ public_html/node/server1.js
@@ -18,10 +18,10 @@ var dispatcher = require('./httpdispatcher');
 				console.log(req.params.smile);
 				client.connect("tcp://127.0.0.1:4242");
 				client.invoke("renderInd", req.params.smile, function(error, res1, more) {
-					res.writeHead(200, {'Content-Type': 'image/png'});
-					var imageData = new Image();
-					imageData.src = 'data:image/png;base64,' + res1;
-					res.end(imageData);
+//					res.writeHead(200, {'Content-Type': 'image/png'});
+//					var imageData = new Image();
+//					imageData.src = 'data:image/png;base64,' + res1;
+					res.end(res1);
 				})
 			}
 		else if (req.params.func == 'hello') {
