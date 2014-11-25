5dbf1ab1ac51414a996cae63153130d7b600f0bd
decilgi0 <gdecil@gmail.com>
2014-11-25 17:07:32 +0100

    / ‘indigo-gdecil/indigo/wsgi/serverTornado0.py’

--- indigo-gdecil/indigo/wsgi/serverTornado0.py
+++ indigo-gdecil/indigo/wsgi/serverTornado0.py
@@ -5,8 +5,13 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("HW")
         
+class WaitHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("pippo")
+
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/wait", WaitHandler),
 ])
 
 if __name__ == "__main__":
