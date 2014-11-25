2759455ae0be7ff45ec1c9ef1e0cdd3b447215f6
decilgi0 <gdecil@gmail.com>
2014-11-25 18:00:51 +0100

    / ‘indigo-gdecil/indigo/wsgi/serverTornado0.py’

--- indigo-gdecil/indigo/wsgi/serverTornado0.py
+++ indigo-gdecil/indigo/wsgi/serverTornado0.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         print 20
-        self.write(datetime.datetime.now().str() )
+        self.write(datetime.now().str() )
         
 class WaitHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
