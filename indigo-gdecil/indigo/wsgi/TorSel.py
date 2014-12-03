1956615d9ec6051e09991064d9ca46ee6712de7d
decilgi0 <gdecil@gmail.com>
2014-12-03 14:45:27 +0100

    / ‘indigo-gdecil/indigo/wsgi/TorSel.py’ / ‘indigo-gdecil/indigo/wsgi/TorSel.pyc’

--- indigo-gdecil/indigo/wsgi/TorSel.py
+++ indigo-gdecil/indigo/wsgi/TorSel.py
@@ -27,6 +27,29 @@ class TornadoSelect(object):
         cur.close()    
         return result
     
+    def renderInd(smile, typeInd):
+        indigo = Indigo()
+        if typeInd == "rea":
+            mol1 = indigo.loadReaction(smile)
+        else:
+            mol1 = indigo.loadMolecule(smile)
+    
+        renderer = IndigoRenderer(indigo);
+        indigo.setOption("render-output-format", "png");
+        indigo.setOption("render-margins", 10, 10);
+        datadir = ""        
+        unique_filename= str(uuid.uuid4()) + ".png"
+    
+        renderer.renderToFile(mol1, datadir + unique_filename);
+            
+        tempFileObj = NamedTemporaryFile(mode='w+b',suffix='png')
+        pilImage = open(datadir + unique_filename,'rb')
+        copyfileobj(pilImage,tempFileObj)
+        pilImage.close()
+        remove(datadir + unique_filename)
+        tempFileObj.seek(0,0)
+        response = send_file(tempFileObj)
+        return response
 
 def get_checkReaEnum():
     try:
@@ -375,41 +398,6 @@ def view_bingo(id):
         return render_template('bingo.html', id=id)
     except TemplateNotFound:
         abort(404)    
-
-def renderInd(smile, typeInd):
-    indigo = Indigo()
-    if typeInd == "rea":
-        mol1 = indigo.loadReaction(smile)
-    else:
-        mol1 = indigo.loadMolecule(smile)
-
-    renderer = IndigoRenderer(indigo);
-    indigo.setOption("render-output-format", "png");
-    indigo.setOption("render-margins", 10, 10);
-    if _platform == "Linux-3.13.0-36-generic-i686-with-Ubuntu-14.04-trusty":
-        datadir = ""        
-    elif _platform == "Linux-3.13.0-39-generic-i686-with-Ubuntu-14.04-trusty":
-        datadir = ""        
-    else:
-        datadir = os.environ['OPENSHIFT_DATA_DIR']
-        
-    unique_filename= str(uuid.uuid4()) + ".png"
-
-    renderer.renderToFile(mol1, datadir + unique_filename);
-        
-    tempFileObj = NamedTemporaryFile(mode='w+b',suffix='png')
-    pilImage = open(datadir + unique_filename,'rb')
-    copyfileobj(pilImage,tempFileObj)
-    pilImage.close()
-    remove(datadir + unique_filename)
-    tempFileObj.seek(0,0)
-    response = send_file(tempFileObj)
-    return response
-
-#     unique_filename= uuid.uuid4()
-#     unique_filename= "mol.png"
-#     print mol1
-#      + ".png"
     
 class User(object):
     def __init__(self,j):
