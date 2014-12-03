5b2463ba42017b74d9a9ae14e50623daf25582f3
decilgi0 <gdecil@gmail.com>
2014-12-03 10:56:53 +0100

    / ‘indigo-gdecil/indigo/wsgi/serverTornadoAsMul.py’

--- indigo-gdecil/indigo/wsgi/serverTornadoAsMul.py
+++ indigo-gdecil/indigo/wsgi/serverTornadoAsMul.py
@@ -54,7 +54,7 @@ def getListpg():
                      for row in cur.fetchall()]
 
     cur.close()    
-    print result
+    print "result"
     return result
     
 class TestHandler(tornado.web.RequestHandler): 
