74288c4325788bab6c044fdb362ee3b931a2a2fd
gippoLinux <gdecil@gmail.com>
2014-12-04 12:42:26 +0100

    / ‘indigo-gdecil/indigo/wsgi/TorSel.py’ / ‘indigo-gdecil/indigo/wsgi/TorSel.pyc’

--- indigo-gdecil/indigo/wsgi/TorSel.py
+++ indigo-gdecil/indigo/wsgi/TorSel.py
@@ -63,7 +63,7 @@ class TornadoSelect(object):
                 '", "isLazy": true , "icon": "/js/vendor/jquery.dynatree/skin-custom/PersonIcon16.gif"},'
                 
             js= js[:-1] + "]"
-            return Response(response=js, status=200, mimetype="application/json")
+            return js
     
         except:
             raise
