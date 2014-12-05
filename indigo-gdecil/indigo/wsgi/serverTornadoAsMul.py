ad297f1097ab96d7992d8f7c9fbc9af769b8d6d0
gippoLinux <gdecil@gmail.com>
2014-12-05 08:38:35 +0100

    / ‘indigo-gdecil/indigo/wsgi/serverTornadoAsMul.py’

--- indigo-gdecil/indigo/wsgi/serverTornadoAsMul.py
+++ indigo-gdecil/indigo/wsgi/serverTornadoAsMul.py
@@ -143,7 +143,7 @@ class Render(tornado.web.RequestHandler):
         self.finish()
 
 class Reaction(tornado.web.RequestHandler): 
-    SUPPORTED_METHODS = RequestHandler.SUPPORTED_METHODS + ('OPTIONS',)
+    SUPPORTED_METHODS = tornado.web.RequestHandler.SUPPORTED_METHODS + ('OPTIONS',)
 #     SUPPORTED_METHODS = ("CONNECT", "GET", "HEAD", "POST", "DELETE", "PATCH", "PUT", "OPTIONS")
     def initialize(self, executor):
         self.executor = executor 
