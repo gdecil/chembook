1f633b80c2886449c6d5323f9199c207195c0fdc
gippoLinux <gdecil@gmail.com>
2014-12-07 18:07:10 +0100

    / ‘indigo-gdecil/indigo/wsgi/TorCfg.py’ / ‘indigo-gdecil/indigo/wsgi/TorCfg.pyc’ < ‘indigo-gdecil/indigo/wsgi/TorCfg.py’ > ‘indigo-gdecil/indigo/wsgi/utility.py’ / ‘indigo-gdecil/indigo/wsgi/utility.pyc’

--- indigo-gdecil/indigo/wsgi/TorCfg.py
+++ indigo-gdecil/indigo/wsgi/TorCfg.py
@@ -1,14 +1,3 @@
 import psycopg2
 global con 
 con = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='postgres', port=5432)
-
-def getParam(dict, name):
-        temp = []
-        dictlist = []
-        for key, value in dict.iteritems():
-            if key == name:
-                return value
-#             temp = [key,value]
-#             dictlist.append(temp)        self.dao = TornadoSelect()
-
-#         print dictlist[0][1]
--- indigo-gdecil/indigo/wsgi/utility.py
+++ indigo-gdecil/indigo/wsgi/utility.py
@@ -3,3 +3,14 @@ import random
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
+def getParam(dict, name):
+        temp = []
+        dictlist = []
+        for key, value in dict.iteritems():
+            if key == name:
+                return value
+#             temp = [key,value]
+#             dictlist.append(temp)        self.dao = TornadoSelect()
+
+#         print dictlist[0][1]
