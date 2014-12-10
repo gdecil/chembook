fe7d985b92fad31ad1d87b253bb39135989bffaf
decilgi0 <gdecil@gmail.com>
2014-12-10 10:41:40 +0100

    / ‘indigo-gdecil/indigo/wsgi/TorSel.py’ / ‘indigo-gdecil/indigo/wsgi/TorSel.pyc’

--- indigo-gdecil/indigo/wsgi/TorSel.py
+++ indigo-gdecil/indigo/wsgi/TorSel.py
@@ -14,7 +14,7 @@ import TorCfg
 class TornadoSelect(object):
     def __init__(self):
         global con 
-        con = TorCfg.con
+        con = TorCfg.conn
 #         con = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='postgres', port=5432)
 
     def getListpg(self):
