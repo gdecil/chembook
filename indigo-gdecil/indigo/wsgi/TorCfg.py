d161610fb8a65931daebbd5b0e63016c0f58dda1
decilgi0 <gdecil@gmail.com>
2014-12-10 10:40:09 +0100

    / ‘indigo-gdecil/indigo/wsgi/TorCfg.py’ / ‘indigo-gdecil/indigo/wsgi/TorCfg.pyc’

--- indigo-gdecil/indigo/wsgi/TorCfg.py
+++ indigo-gdecil/indigo/wsgi/TorCfg.py
@@ -1,3 +1,12 @@
 import psycopg2
 global con 
-con = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='postgres', port=5432)
+conn_string = "host='127.0.0.1' dbname='postgres' user='postgres' password='postgres' port=5433"
+conn = psycopg2.connect(conn_string)
+
+def query_db(query, args=(), one=False):
+    cur = conn.cursor()
+    cur.execute(query, args)
+    r = [dict((cur.description[i][0], value) \
+               for i, value in enumerate(row)) for row in cur.fetchall()]
+#     cur.connection.close()
+    return (r[0] if r else None) if one else r
