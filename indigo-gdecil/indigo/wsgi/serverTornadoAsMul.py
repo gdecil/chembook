588063a9dbde6f4dd4db78ccf12ac5e35227350d
decilgi0 <gdecil@gmail.com>
2014-12-15 10:37:13 +0100

    / ‘indigo-gdecil/indigo/wsgi/serverTornadoAsMul.py’

--- indigo-gdecil/indigo/wsgi/serverTornadoAsMul.py
+++ indigo-gdecil/indigo/wsgi/serverTornadoAsMul.py
@@ -205,6 +205,7 @@ class Reaction(tornado.web.RequestHandler):
                 a0 = self.request.body
                 a00= a0.replace('\n','\\n')
                 a1= tornado.escape.json_decode(a00)
+                print a0
                 print a1
             elif param1 == 'GetProductsIndigo' or param1 == 'GetReagentsIndigo':
                 a0 = self.request.body
